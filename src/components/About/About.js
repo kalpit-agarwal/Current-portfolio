2b38f7dd445da4a2d42a1b85888c45d7b0eb4028
kalpit-agarwal <108914877+kalpit-agarwal@users.noreply.github.com>
2024-06-20 17:07:03 +0530

    first commit

---
+++ src/components/About/About.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+import ScrollAnimation from "react-animate-on-scroll";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+          <ScrollAnimation animateIn="fadeInLeft">
+            <Image src="/aboutphoto.jpg" alt="man-svgrepo" />
+          </ScrollAnimation>
+          <div className="AboutBio">
+            <ScrollAnimation animateIn="fadeInLeft">
+              Hello! My name is <strong>Kalpit agarwal</strong>. I'm originally
+              Udaipur, rajasthan but i am currently pursuing my bachelor's
+              degree from LNMIIT, Jaipur . During my time here, I gained
+              valuable experience through various workshops and interactions
+              with different personalities, which greatly enhanced my work
+              ethic, communication skills, and adaptability.
+            </ScrollAnimation>
+
+            <br />
+            <br />
+
+            <ScrollAnimation animateIn="fadeInLeft">
+              My journey into the world of technology began with a strong
+              passion for problem-solving, I love solving competitive coding
+              problems for fun. Despite its challenges, I remained determined
+              and forged lasting friendships that supported me throughout this
+              interesting journey. Throughout my studies at LNMIIT, I had the
+              opportunity to engage in fascinating projects. Notably, I
+              contributed to the development of the super coach and BBI api in
+              my SDE intership time at Octal IT solution
+            </ScrollAnimation>
+
+            <br />
+            <br />
+
+            <ScrollAnimation animateIn="fadeInLeft">
+              Working collaboratively within a team towards a shared objective
+              has been an incredibly rewarding and unique experience for me. I
+              am eager to continue exploring exciting projects in the future,
+              with a particular interest in web development and cross-platform
+              mobile development.
+              <br />
+              <br />
+              Here are my competitive coding profiles:
+              <br />
+              <a
+                style={{ color: "white", backgroundColor: "black" }}
+                href="https://codeforces.com/profile/thunder_fart"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Codeforces
+              </a>
+              <br />
+              <a
+                style={{ color: "white", backgroundColor: "black" }}
+                href="https://leetcode.com/u/kalpit0605/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Leetcode
+              </a>
+              <div className="tagline2">
+                I have become confident using the following technologies:
+              </div>
+            </ScrollAnimation>
+
+            <Technologies>
+              {stackList.map((stack, index) => (
+                <ScrollAnimation animateIn="fadeInLeft" key={index}>
+                  <Tech key={index} className="tech">
+                    <TechImg src={stack.img} alt={stack.name} />
+                    <TechName>{stack.name}</TechName>
+                  </Tech>
+                </ScrollAnimation>
+              ))}
+            </Technologies>
+          </div>
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
---
+++ src/data/ProjectData.js
@@ -0,0 +1,165 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/savorify.png",
+    title: "Savorify",
+    description:
+      "A fully functional food delivery app that allows users to order food from their favourite restaurants." +
+      "It also allows restaurant owners to manage their menu and orders.",
+    tech_stack: [
+      "React",
+      "NodeJs",
+      "MongoDB",
+      "ReactContext",
+      "Stripe",
+      "ExpressJs",
+    ],
+    github_url: "https://github.com/kalpit-agarwal/Savorify",
+    demo_url: "https://savorify.netlify.app/",
+  },
+  {
+    img: "/supercoach.png",
+    title: "SuperCoach",
+    description:
+      "An app for athletes and coaches to connect and train together. It allows coaches to create training plans and athletes to follow them.",
+    tech_stack: [
+      "HTML",
+      "CSS",
+      "NodeJs",
+      "MongoDB",
+      "Reactjs",
+      "ExpressJs",
+      "JWT",
+      "Material UI",
+      "React-hook-form",
+    ],
+    github_url: "https://github.com/kalpit-agarwal/super-coach-backend",
+    demo_url: "https://super-coach.netlify.app/",
+  },
+
+  {
+    img: "/blogisite.png",
+    title: "Blogisite",
+    description:
+      "A website designed beatuifully to showcase blogs and articles.",
+    tech_stack: ["React ", "Font awesome", "React Router", "React Hooks"],
+    github_url: "https://github.com/kalpit-agarwal/BlogiSite",
+    demo_url: "https://blogisite.netlify.app/",
+  },
+  {
+    img: "/textchanger.png",
+    title: "TextChanger",
+    description: "A basic project for practising react with basic logics.",
+    tech_stack: ["ReactJs", "JavaScript"],
+    github_url: "https://github.com/kalpit-agarwal/textChanger-React",
+    demo_url: "https://text-changer-react.vercel.app/",
+  },
+  {
+    img: "/ML.jpg",
+    title: "SciKit-Learn",
+    description:
+      "Project to learn and implement machine learning algorithms using scikit-learn.",
+    tech_stack: [
+      "Python",
+      "Scikit-learn",
+      "Jupyter Notebook",
+      "Pandas",
+      "Numpy",
+    ],
+    github_url: "https://github.com/bajajdivya/mnist_t",
+    demo_url: "",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
+  //   name: "NextJs",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+  //   name: "Typescript",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+  //   name: "React Native",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
+  //   name: "Firebase",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flask/flask-original.svg",
+  //   name: "Flask",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  {
+    img: "https://pluralsight.imgix.net/paths/path-icons/c-plus-plus-93c7ddd5cc.png",
+    name: "C++  ",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+  //   name: "Java",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+  //   name: "MySQL",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+    name: "Material UI",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
+    name: "Figma",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-plain.svg",
+  //   name: "TailwindCSS",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
